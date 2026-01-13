02597a7b250de5efa5d72aedf6d9946c46bc30ed
kapsevarun15-gif <kapsevarun15@gmail.com>
2026-01-13 14:35:26 +0530

    today's code

---
+++ javascript/hello.js
@@ -0,0 +1,31 @@
+
+//  let age = 24
+
+//  let name = "varun" 
+
+// const city = "nashik"
+
+// console.log(name)
+
+function hello(){
+    console.log("hello world");
+
+}
+
+hello()
+
+let arr = [1,2,3,4]
+console.log(arr)
+
+arr.push("varun")
+
+console.log(arr)
+
+// object
+let student=
+{
+    name : "varun",
+    age : 21
+}
+
+console.log(student.name)
